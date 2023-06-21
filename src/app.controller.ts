70369be20b1cf6882fe228b25fbb5532f6ec3af9
Kennytian <kt.tian@gmail.com>
2023-06-21 11:04:48 +0800

    feat: initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Headers } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('AppController')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Hello' })
   @Get()
-  getHello(): string {
+  getHello(@Headers() headers): string {
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    .setTitle('Api Title')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
+
+  console.log(
+    `api service listening at http://127.0.0.1:3000/api, the time now: ${new Date().toLocaleString()}`,
+  );
 }
 bootstrap();
